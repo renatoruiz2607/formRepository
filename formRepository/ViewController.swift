b32ef22e014655e18832e74f35aaf071a1e0060b
renatoruiz2607 <renatoruiz2607@gmail.com>
2021-08-09 09:02:36 -0300

    criada validacao automatica conforme digita

--- formRepository/ViewController.swift
+++ formRepository/ViewController.swift
@@ -19,14 +19,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setup()
     }
-
+    
     func setup() {
+        nameTextField.delegate = self
+        CPFTextField.delegate = self
         phoneTextField.delegate = self
+        emailTextField.delegate = self
     }
 }
 
 extension ViewController: UITextFieldDelegate {
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let name = nameTextField.text {
+            let letterCharacters = CharacterSet.letters
+            if (name.rangeOfCharacter(from: letterCharacters) != nil) && name.count >= 3{
+                nameTextField.backgroundColor = UIColor.systemGreen
+            } else if name == "" {
+                nameTextField.backgroundColor = UIColor.clear
+            } else {
+                nameTextField.backgroundColor = UIColor.systemRed
+            }
+        }
+        if let cpf = CPFTextField.text {
+            if CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: cpf)) && cpf.count == 11 {
+                CPFTextField.backgroundColor = UIColor.systemGreen
+            } else if cpf == "" {
+                CPFTextField.backgroundColor = UIColor.clear
+            } else {
+                CPFTextField.backgroundColor = UIColor.systemRed
+            }
+        }
         if let phone = phoneTextField.text {
             if CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: phone)) && phone.count == 11 {
                 phoneTextField.backgroundColor = UIColor.systemGreen
@@ -34,6 +56,39 @@ extension ViewController: UITextFieldDelegate {
                 phoneTextField.backgroundColor = UIColor.clear
             } else {
                 phoneTextField.backgroundColor = UIColor.systemRed
+            }
+        }
+        if let email = emailTextField.text {
+            if email.contains("@") && email.count >= 8 {
+                emailTextField.backgroundColor = UIColor.systemGreen
+            } else if email == "" {
+                emailTextField.backgroundColor = UIColor.clear
+            } else {
+                emailTextField.backgroundColor = UIColor.systemRed
+            }
+        }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if let name = nameTextField.text {
+            if name == "" {
+                nameTextField.backgroundColor = UIColor.clear
+            }
+        }
+        if let phone = phoneTextField.text {
+            if phone == "" {
+                phoneTextField.backgroundColor = UIColor.clear
+            }
+        }
+        if let cpf = CPFTextField.text {
+            if cpf == "" {
+                CPFTextField.backgroundColor = UIColor.clear
+            }
+        }
+        if let email = emailTextField.text {
+            if email == "" {
+                emailTextField.backgroundColor = UIColor.clear
             }
         }
         return true
